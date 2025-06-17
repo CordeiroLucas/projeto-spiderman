7d7d554242c349e3bb0763a6a8d5058322f10785
CordeiroLucas <lcp2@cesar.school>
2025-06-17 17:03:33 -0300

    feat: Title Hover Animation

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -2,12 +2,17 @@ function handleMouseEnter() {
     this.classList.add('s-card--hovered')
 }
 
+function handleMouseLeave() {
+    this.classList.remove('s-card--hovered')
+}
+
 function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('s-card')
 
     for (let index = 0; index < cardElements.length; index++) {
         const card = cardElements[index];
         card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
     }
 }
 
