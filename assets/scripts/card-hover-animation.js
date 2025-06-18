eba81fac95dddaa8eb139c4ee8822dd69e2e13fb
CordeiroLucas <lcp2@cesar.school>
2025-06-18 11:09:48 -0300

    feat: Background Animation

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,9 +1,11 @@
 function handleMouseEnter() {
-    this.classList.add('s-card--hovered')
+    this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`;
 }
 
 function handleMouseLeave() {
-    this.classList.remove('s-card--hovered')
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
 }
 
 function addEventListenersToCards() {
